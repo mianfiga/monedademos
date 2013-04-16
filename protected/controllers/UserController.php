855d2852ffe5c1a96e097d6cff61a65741da2c28
Miguel Ángel Figueroa García <miguel.angel.fg@gmail.com>
2013-04-16 15:17:46 +0100

    Mobile Sign Up correction

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -1,273 +1,257 @@
 <?php
 
-class UserController extends Controller
-{
-	/**
-	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
-	 * using two-column layout. See 'protected/views/layouts/column2.php'.
-	 */
-	public $layout='//layouts/column2';
+class UserController extends Controller {
 
-	public function actions()
-	{
-		return array(
-			// captcha action renders the CAPTCHA image displayed on the contact page
-			'captcha'=>array(
-				'class'=>'CCaptchaAction',
-				'backColor'=>0xFFFFFF,
-			));
-	}
-	/**
-	 * @return array action filters
-	 */
-	public function filters()
-	{
-		return array(
-			'accessControl', // perform access control for CRUD operations
-		);
-	}
+    /**
+     * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+     * using two-column layout. See 'protected/views/layouts/column2.php'.
+     */
+    public $layout = '//layouts/column2';
 
-	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
-	public function accessRules()
-	{
-		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('captcha','invited','create'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('index','view','update','edit'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('index','delete','admin'),
-				'users'=>array('admin'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
-		);
-	}
+    public function actions() {
+        return array(
+            // captcha action renders the CAPTCHA image displayed on the contact page
+            'captcha' => array(
+                'class' => 'CCaptchaAction',
+                'backColor' => 0xFFFFFF,
+                ));
+    }
 
-	/**
-	 * Displays a particular model.
-	 * @param integer $id the ID of the model to be displayed
-	 */
-	public function actionView($id)
-	{
-		if (Yii::app()->user->getId()==$id)
-			$this->render('view',array(
-				'model'=>$this->loadModel($id),
-			));
-		else
-			$this->redirect(array('site/index'));
-	}
+    /**
+     * @return array action filters
+     */
+    public function filters() {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+        );
+    }
 
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules() {
+        return array(
+            array('allow', // allow all users to perform 'index' and 'view' actions
+                'actions' => array('captcha', 'invited', 'create'),
+                'users' => array('*'),
+            ),
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                'actions' => array('index', 'view', 'update', 'edit'),
+                'users' => array('@'),
+            ),
+            array('allow', // allow admin user to perform 'admin' and 'delete' actions
+                'actions' => array('index', 'delete', 'admin'),
+                'users' => array('admin'),
+            ),
+            array('deny', // deny all users
+                'users' => array('*'),
+            ),
+        );
+    }
 
-	/**
-	 * Creates a new model.
-	 * If creation is successful, the browser will be redirected to the 'view' page.
-	 */
-	public function actionInvited($id,$code)
-	{
+    /**
+     * Displays a particular model.
+     * @param integer $id the ID of the model to be displayed
+     */
+    public function actionView($id) {
+        if (Yii::app()->user->getId() == $id)
+            $this->render('view', array(
+                'model' => $this->loadModel($id),
+            ));
+        else
+            $this->redirect(array('site/index'));
+    }
+
+    /**
+     * Creates a new model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     */
+    public function actionInvited($id, $code) {
 
 //buscamos la invitación
-		$invitation=Invitation::model()->findByPk($id);
+        $invitation = Invitation::model()->findByPk($id);
 //comprobamos que sea correcta
-		if ($invitation->code != $code)
-			throw new CHttpException(404,'Invalid invitation');
+        if ($invitation->code != $code)
+            throw new CHttpException(404, 'Invalid invitation');
 //comprobamos que no haya sido usada ya
-		if ($invitation->used)
-			throw new CHttpException(404,'Invitation already used');
+        if ($invitation->used)
+            throw new CHttpException(404, 'Invitation already used');
 //comprobamos que no haya caducado
-		if ($invitation->created < date('Y-m-d H:i:s',strtotime("-".Invitation::EXPIRATION." days")))
-			$this->redirect(array('create'));
+        if ($invitation->created < date('Y-m-d H:i:s', strtotime("-" . Invitation::EXPIRATION . " days")))
+            $this->redirect(array('create'));
 //mostramos el formulario y creamos el usuario
 
-		$model=new User('register');
+        $model = new User('register');
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
 
-		if(isset($_POST['User']))
-		{
-			$model->attributes=$_POST['User'];
-			$model->created_by=$invitation->user_id;
-			if($model->save())
-			{
-				$invitation->used = date('YmdHis');
-				$invitation->save();
-				$this->redirect(array('view','id'=>$model->id));
-			}
-		}
+        if (isset($_POST['User'])) {
+            $model->attributes = $_POST['User'];
+            $model->created_by = $invitation->user_id;
+            if ($model->save()) {
+                $invitation->used = date('YmdHis');
+                $invitation->save();
+                $this->redirect(array('view', 'id' => $model->id));
+            }
+        }
 
-		$this->render('create',array(
-			'model'=>$model,
-		));
-	}
+        $this->render('create', array(
+            'model' => $model,
+        ));
+    }
 
-	/**
-	 * Creates a new model.
-	 * If creation is successful, the browser will be redirected to the 'view' page.
-	 */
-	public function actionCreate()
-	{
-        $this->redirect(array('site/login'));
-		$model=new User('register');
+    /**
+     * Creates a new model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     */
+    public function actionCreate() {
+        if (Yii::app()->user->getId()) {
+            Yii::app()->user->setFlash('notice', Yii::t('app', 'You are already signed in, to sign in with another user first Logout (in the top menu)'));
+            $this->redirect(array(Yii::app()->defaultController));
+        }
+        $this->layout = '//layouts/column1';
+        
+        $model = new User('register');
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
 
-		if(isset($_POST['User']))
-		{
-			$model->attributes=$_POST['User'];
-			if($model->save())
-				$this->redirect(array(Yii::app()->defaultController));
-		}
+        if (isset($_POST['User'])) {
+            $model->attributes = $_POST['User'];
+            if ($model->save())
+                $this->redirect(array(Yii::app()->defaultController));
+        }
 
-		$this->render('create',array(
-			'model'=>$model,
-		));
-	}
+        $this->render('create', array(
+            'model' => $model,
+        ));
+    }
 
-	/**
-	 * Updates a particular model.
-	 * If update is successful, the browser will be redirected to the 'view' page.
-	 * @param integer $id the ID of the model to be updated
-	 */
-	public function actionEdit($id)
-	{
+    /**
+     * Updates a particular model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id the ID of the model to be updated
+     */
+    public function actionEdit($id) {
 
-		if (Yii::app()->user->getId()!=$id)
-			$this->redirect(array('site/index'));
+        if (Yii::app()->user->getId() != $id)
+            $this->redirect(array('site/index'));
 
-		$model=$this->loadModel($id);
-		$model->setScenario('edit');
+        $model = $this->loadModel($id);
+        $model->setScenario('edit');
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
 
-		if(isset($_POST['User']))
-		{
-			$model->attributes=$_POST['User'];
-      $model->updated = date('YmdHis');
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
-		}
+        if (isset($_POST['User'])) {
+            $model->attributes = $_POST['User'];
+            $model->updated = date('YmdHis');
+            if ($model->save())
+                $this->redirect(array('view', 'id' => $model->id));
+        }
 
-		$this->render('update',array(
-			'model'=>$model,
-		));
-	}
+        $this->render('update', array(
+            'model' => $model,
+        ));
+    }
 
+    /**
+     * Updates a particular model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id the ID of the model to be updated
+     */
+    public function actionUpdate($id) {
+        if (Yii::app()->user->getId() != $id)
+            $this->redirect(array('site/index'));
 
-	/**
-	 * Updates a particular model.
-	 * If update is successful, the browser will be redirected to the 'view' page.
-	 * @param integer $id the ID of the model to be updated
-	 */
-	public function actionUpdate($id)
-	{
-		if (Yii::app()->user->getId()!=$id)
-			$this->redirect(array('site/index'));
-
-		$model=$this->loadModel($id);
-		$model->setScenario('update');
+        $model = $this->loadModel($id);
+        $model->setScenario('update');
 //		$model->password = null;
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+        if (isset($_POST['User'])) {
+            $model->attributes = $_POST['User'];
+            $model->updated = date('YmdHis');
+            if ($model->save())
+                $this->redirect(array('view', 'id' => $model->id));
+        }
 
-		if(isset($_POST['User']))
-		{
-			$model->attributes=$_POST['User'];
-      $model->updated = date('YmdHis');
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
-		}
+        $this->render('update', array(
+            'model' => $model,
+        ));
+    }
 
-		$this->render('update',array(
-			'model'=>$model,
-		));
-	}
+    /**
+     * Deletes a particular model.
+     * If deletion is successful, the browser will be redirected to the 'admin' page.
+     * @param integer $id the ID of the model to be deleted
+     */
+    public function actionDelete($id) {
+        if (Yii::app()->user->getId() != $id)
+            $this->redirect(array('site/index'));
 
-	/**
-	 * Deletes a particular model.
-	 * If deletion is successful, the browser will be redirected to the 'admin' page.
-	 * @param integer $id the ID of the model to be deleted
-	 */
-	public function actionDelete($id)
-	{
-		if (Yii::app()->user->getId()!=$id)
-			$this->redirect(array('site/index'));
+        if (Yii::app()->request->isPostRequest) {
+            // we only allow deletion via POST request
+            $this->loadModel($id)->delete();
 
-		if(Yii::app()->request->isPostRequest)
-		{
-			// we only allow deletion via POST request
-			$this->loadModel($id)->delete();
+            // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+            if (!isset($_GET['ajax']))
+                $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+        }
+        else
+            throw new CHttpException(400, 'Invalid request. Please do not repeat this request again.');
+    }
 
-			// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-			if(!isset($_GET['ajax']))
-				$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-		}
-		else
-			throw new CHttpException(400,'Invalid request. Please do not repeat this request again.');
-	}
+    /**
+     * Lists all models.
+     */
+    public function actionIndex() {
+        $this->redirect(array('view', 'id' => Yii::app()->user->getId()));
+        /* 		$dataProvider=new CActiveDataProvider('User');
+          $this->render('index',array(
+          'dataProvider'=>$dataProvider,
+          )); */
+    }
 
-	/**
-	 * Lists all models.
-	 */
-	public function actionIndex()
-	{
-		$this->redirect(array('view','id'=>Yii::app()->user->getId()));
-/*		$dataProvider=new CActiveDataProvider('User');
-		$this->render('index',array(
-			'dataProvider'=>$dataProvider,
-		));*/
-	}
+    /**
+     * Manages all models.
+     */
+    public function actionAdmin() {
+        $model = new User('search');
+        $model->unsetAttributes();  // clear any default values
+        if (isset($_GET['User']))
+            $model->attributes = $_GET['User'];
 
-	/**
-	 * Manages all models.
-	 */
-	public function actionAdmin()
-	{
-		$model=new User('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['User']))
-			$model->attributes=$_GET['User'];
+        $this->render('admin', array(
+            'model' => $model,
+        ));
+    }
 
-		$this->render('admin',array(
-			'model'=>$model,
-		));
-	}
+    /**
+     * Returns the data model based on the primary key given in the GET variable.
+     * If the data model is not found, an HTTP exception will be raised.
+     * @param integer the ID of the model to be loaded
+     */
+    public function loadModel($id) {
+        $model = User::model()->findByPk($id);
+        if ($model === null)
+            throw new CHttpException(404, 'The requested page does not exist.');
+        return $model;
+    }
 
-	/**
-	 * Returns the data model based on the primary key given in the GET variable.
-	 * If the data model is not found, an HTTP exception will be raised.
-	 * @param integer the ID of the model to be loaded
-	 */
-	public function loadModel($id)
-	{
-		$model=User::model()->findByPk($id);
-		if($model===null)
-			throw new CHttpException(404,'The requested page does not exist.');
-		return $model;
-	}
+    /**
+     * Performs the AJAX validation.
+     * @param CModel the model to be validated
+     */
+    protected function performAjaxValidation($model) {
+        if (isset($_POST['ajax']) && $_POST['ajax'] === 'user-form') {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+    }
 
-	/**
-	 * Performs the AJAX validation.
-	 * @param CModel the model to be validated
-	 */
-	protected function performAjaxValidation($model)
-	{
-		if(isset($_POST['ajax']) && $_POST['ajax']==='user-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
-	}
 }
