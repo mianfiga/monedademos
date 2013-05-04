d8223aa35d3e5cc8e4ab19092c9f0d920247ed64
Miguel Ángel Figueroa García <miguel.angel.fg@gmail.com>
2013-05-05 00:53:59 +0100

    Added rates to transaction/view

--- protected/views/contribution/_contributions.php
+++ protected/views/contribution/_contributions.php
@@ -1,11 +1,53 @@
-<h3><?php echo Yii::t('app','Client contribution')?></h3>
 <?php
-	$client = Authorization::splitAccountNumber($model->getChargeAccountNumber());
-	echo $this->renderPartial('//contribution/_view', array('data'=>User::model()->findByPk($client['entity_id'])));
+/* @var $this contributionController */
+/* @var $model Transaction */
 ?>
 
-<h3><?php echo Yii::t('app','Vendor contribution')?></h3>
+<div class="row">
+    <div class="small-12 large-4 columns">
+        <h3><?php echo Yii::t('app', 'Client contribution') ?></h3>
+    </div>
+    <div class="small-12 large-8 columns">
+        <?php
+        $this->widget('CStarRating', array(
+            'name' => 'depositRating',
+            'model' => $model->depositEntity,
+            'attribute' => 'rate',
+            'minRating' => 1,
+            'maxRating' => 5,
+            'starCount' => 5,
+            'readOnly' => true,
+            'starWidth' => '64',
+        ));
+        echo '&nbsp;&nbsp;' . Yii::t('app', 'In {count} rates', array('{count}' => $model->depositEntity->rates));
+        ?>
+    </div>
+</div>
 <?php
-	$vendor = Authorization::splitAccountNumber($model->getDepositAccountNumber());
-	echo $this->renderPartial('//contribution/_view', array('data'=>User::model()->findByPk($vendor['entity_id'])));
+echo $this->renderPartial('//contribution/_view', array('data' => $model->depositEntity->getObject()));
+?>
+<div class="row">
+    <div class="small-12 large-4 columns">
+
+        <h3><?php echo Yii::t('app', 'Vendor contribution') ?></h3>
+    </div>
+    <div class="small-12 large-8 columns">
+
+        <?php
+        $this->widget('CStarRating', array(
+            'name' => 'chargeRating',
+            'model' => $model->chargeEntity,
+            'attribute' => 'rate',
+            'minRating' => 1,
+            'maxRating' => 5,
+            'starCount' => 5,
+            'readOnly' => true,
+            'starWidth' => '64',
+        ));
+        echo '&nbsp;&nbsp;' . Yii::t('app', 'In {count} rates', array('{count}' => $model->chargeEntity->rates));
+        ?>
+    </div>
+</div>
+<?php
+echo $this->renderPartial('//contribution/_view', array('data' => $model->chargeEntity->getObject()));
 ?>
