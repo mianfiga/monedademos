0257bd28c41aca6755f053ddd94a2101f76bd61b
Miguel Ángel Figueroa García <miguel.angel.fg@gmail.com>
2013-11-25 01:39:06 +0000

    Translated update user message

--- protected/views/user/update.php
+++ protected/views/user/update.php
@@ -15,6 +15,6 @@ $this->menu=array(
 );
 ?>
 
-<h1>Update User <?php echo $model->username; ?></h1>
+<h1><?php Yii::t('app','Update user info'); ?></h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
