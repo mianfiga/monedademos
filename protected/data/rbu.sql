39ede0535babd53b9b9db292954e7f03a46de22e
Miguel Ángel Figueroa García <miguel.angel.fg@gmail.com>
2017-12-19 21:31:53 +0000

    First steps fixed 2

--- protected/data/rbu.sql
+++ protected/data/rbu.sql
@@ -17,7 +17,7 @@ CREATE TABLE `rbu_account` (
   `title` varchar(127) DEFAULT NULL,
   `access` enum('private','public') NOT NULL DEFAULT 'private',
   `added` timestamp NOT NULL,
-  `last_action` timestamp,
+  `last_action` timestamp NULL,
   `blocked` timestamp NULL DEFAULT NULL,
   `deleted` timestamp NULL DEFAULT NULL,
   `total_earned` bigint(20) unsigned NOT NULL DEFAULT '0',
@@ -62,8 +62,8 @@ CREATE TABLE `rbu_authorization` (
   `code` char(1) NOT NULL,
   `class` enum('holder','authorized','consultant') NOT NULL DEFAULT 'holder',
   `title` varchar(127) DEFAULT NULL,
-  `salt` varchar(128) NOT NULL,
-  `password` varchar(128) NOT NULL,
+  `salt` varchar(128) NOT NULL DEFAULT '',
+  `password` varchar(128) NOT NULL DEFAULT '',
   `wrong_pass_count` tinyint(4) NOT NULL DEFAULT '0',
   `added` timestamp NOT NULL,
   `blocked` timestamp NULL DEFAULT NULL,
@@ -85,7 +85,7 @@ CREATE TABLE `rbu_brand` (
   `summary` text,
   `description` text,
   `image` varchar(254) DEFAULT NULL,
-  `last_action` timestamp,
+  `last_action` timestamp NULL,
   `created_by` int(10) unsigned DEFAULT NULL,
   `culture` varchar(7) NOT NULL DEFAULT 'es_es',
   `added` timestamp NOT NULL,
@@ -102,14 +102,16 @@ CREATE TABLE `rbu_brand` (
 --
 
 CREATE TABLE `rbu_entity` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `class` varchar(32) NOT NULL,
-  `object_id` int(11) unsigned NOT NULL,
+  `object_id` int(11) UNSIGNED NOT NULL,
   `points` int(11) NOT NULL DEFAULT '0',
   `rates` int(11) NOT NULL DEFAULT '0',
-  `tribe_id` int(11) unsigned DEFAULT NULL,
+  `last_transaction` timestamp NULL DEFAULT NULL,
+  `magic` varchar(64) DEFAULT NULL,
+  `tribe_id` int(11) UNSIGNED DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `tribe_id` (`tribe_id`)
+  KEY `rbu_entity_tribe_id` (`tribe_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- --------------------------------------------------------
@@ -156,7 +158,7 @@ CREATE TABLE IF NOT EXISTS `rbu_tribe` (
   `summary` text,
   `description` text,
   `image` varchar(254) DEFAULT NULL,
-  `last_action` timestamp,
+  `last_action` timestamp NULL,
   `group_id` int(10) unsigned DEFAULT NULL,
   `created_by` int(10) unsigned DEFAULT NULL,
   `added` timestamp NOT NULL,
@@ -249,6 +251,7 @@ CREATE TABLE `rbu_market_ad` (
   `expiration` date NOT NULL,
   `zip` varchar(16) DEFAULT NULL,
   `created_by` int(10) unsigned DEFAULT NULL,
+  `anonymous` tinyint(1) NOT NULL DEFAULT '1',
   `added` timestamp NOT NULL,
   `updated` timestamp NOT NULL,
   PRIMARY KEY (`id`),
@@ -446,12 +449,14 @@ CREATE TABLE `rbu_rule` (
 CREATE TABLE `rbu_transaction` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `executed_at` timestamp NOT NULL,
-  `class` enum('salary','tax','transfer','charge','movement','system') NOT NULL,
+  `class` enum('salary','tax','transfer','charge','movement','system','refund','system refund') NOT NULL,
   `amount` bigint(20) unsigned NOT NULL,
   `charge_account` int(10) unsigned NOT NULL,
   `deposit_account` int(10) unsigned NOT NULL,
   `charge_entity` int(11) unsigned NOT NULL,
   `deposit_entity` int(11) unsigned NOT NULL,
+  `charge_tribe` int(11) UNSIGNED NOT NULL DEFAULT '1',
+  `deposit_tribe` int(11) UNSIGNED NOT NULL DEFAULT '1',
   `subject` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   KEY `FK_transaction_charge_account` (`charge_account`),
@@ -478,15 +483,17 @@ CREATE TABLE `rbu_user` (
   `contribution_title` varchar(255) NOT NULL,
   `contribution_text` text NOT NULL,
   `email` varchar(255) NOT NULL,
-  `contact` text NOT NULL,
+  `contact` text NOT NULL DEFAULT '',
   `zip` varchar(16) NOT NULL,
+  `image` varchar(254) COLLATE utf8_spanish2_ci DEFAULT NULL,
   `country` varchar(4) NOT NULL DEFAULT 'ES',
   `culture` varchar(7) NOT NULL DEFAULT 'es_es',
+  `abilities` set('invite') COLLATE utf8_spanish2_ci DEFAULT NULL,
   `exemption_id` int(10) unsigned DEFAULT NULL,
   `created` timestamp NOT NULL,
   `created_by` int(10) unsigned DEFAULT NULL,
-  `last_login` timestamp NOT NULL,
-  `last_action` timestamp NOT NULL,
+  `last_login` timestamp NULL,
+  `last_action` timestamp NULL,
   `updated` datetime NOT NULL,
   `magic` varchar(64) DEFAULT NULL,
   `blocked` timestamp NULL DEFAULT NULL,
@@ -497,6 +504,35 @@ CREATE TABLE `rbu_user` (
   KEY `FK_user_created_by` (`created_by`),
   KEY `FK_exemption_exemption_id` (`exemption_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+
+--
+-- Estructura de tabla para la tabla `rbu_market_ad_tribe`
+--
+
+CREATE TABLE `rbu_market_ad_tribe` (
+  `ad_id` bigint(20) UNSIGNED NOT NULL,
+  `tribe_id` int(11) UNSIGNED NOT NULL,
+  `added` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+
+
+--
+-- Estructura de tabla para la tabla `rbu_api_telegram`
+--
+
+CREATE TABLE `rbu_api_telegram` (
+  `entity_id` int(11) UNSIGNED NOT NULL,
+  `chat_id` varchar(64) COLLATE utf8_spanish2_ci NOT NULL,
+  `user_id` varchar(64) COLLATE utf8_spanish2_ci NOT NULL,
+  `username` varchar(64) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `market_notifications` tinyint(1) NOT NULL DEFAULT '0',
+  `last_action` timestamp NULL DEFAULT NULL,
+  `added` timestamp NULL DEFAULT NULL,
+  `updated` timestamp NULL DEFAULT NULL,
+  `deleted` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Filtros para las tablas descargadas (dump)
@@ -638,8 +674,38 @@ ALTER TABLE `rbu_user`
   ADD CONSTRAINT `FK_exemption_exemption_id` FOREIGN KEY (`exemption_id`) REFERENCES `rbu_exemption` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `FK_user_created_by` FOREIGN KEY (`created_by`) REFERENCES `rbu_user` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
+--
+-- Indices de la tabla `rbu_market_ad_tribe`
+--
+ALTER TABLE `rbu_market_ad_tribe`
+  ADD PRIMARY KEY (`ad_id`,`tribe_id`),
+  ADD KEY `FK_market_ad_tribe_tribe` (`tribe_id`);
+
+--
+-- Filtros para la tabla `rbu_market_ad_tribe`
+--
+ALTER TABLE `rbu_market_ad_tribe`
+  ADD CONSTRAINT `FK_market_ad_tribe_ad` FOREIGN KEY (`ad_id`) REFERENCES `rbu_market_ad` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_market_ad_tribe_tribe` FOREIGN KEY (`tribe_id`) REFERENCES `rbu_tribe` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 SET FOREIGN_KEY_CHECKS=1;
+
+--
+-- Indices de la tabla `rbu_api_telegram`
+--
+ALTER TABLE `rbu_api_telegram`
+  ADD PRIMARY KEY (`entity_id`),
+  ADD UNIQUE KEY `chat_id` (`chat_id`),
+  ADD UNIQUE KEY `user_id` (`user_id`);
+
+--
+-- Filtros para la tabla `rbu_api_telegram`
+--
+ALTER TABLE `rbu_api_telegram`
+  ADD CONSTRAINT `FK_telegram_entity_id` FOREIGN KEY (`entity_id`) REFERENCES `rbu_entity` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+
+
 
 
 
@@ -656,7 +722,7 @@ INSERT INTO `rbu_tribe_group` (`id`) VALUES
 --
 
 INSERT INTO `rbu_tribe` (`id`, `nickname`, `name`, `email`, `summary`, `description`, `image`, `last_action`, `group_id`, `created_by`, `added`, `updated`, `deleted`) VALUES
-(1, 'default', 'La Tribu por defecto de Demos', 'contacto@monedademos.es', 'Al llegar a demos entrarás a esta tribu hasta que encuentres o crees un grupo con el que empezar a participar. Esta isla no es una isla de pruebas, lo que hagas aquí formará parte de tu actividad en Demos', NULL, NULL, '0000-00-00 00:00:00', 1, NULL, '2014-04-24 16:00:56', '2014-04-24 16:00:56', NULL);
+(1, 'default', 'La Tribu por defecto de Demos', 'contacto@monedademos.es', 'Al llegar a demos entrarás a esta tribu hasta que encuentres o crees un grupo con el que empezar a participar. Esta isla no es una isla de pruebas, lo que hagas aquí formará parte de tu actividad en Demos', NULL, NULL, now(), 1, NULL, now(), now(), NULL);
 
 
 --
@@ -671,8 +737,8 @@ INSERT INTO `rbu_entity` (`id`, `class`, `object_id`, `points`, `rates`, `tribe_
 --
 
 INSERT INTO `rbu_account` (`id`, `tribe_id`, `class`, `credit`, `earned`, `spended`, `balance`, `title`, `access`, `added`, `last_action`, `blocked`, `deleted`, `total_earned`, `total_spended`, `total_clients`, `total_sellers`, `best_clients`, `best_sellers`, `deposit_transfer_count`, `charge_transfer_count`) VALUES
-(1, 1, 'fund', 5000000, 0, 0, 0, 'System Fund, where the non asigned money stays', 'public', now(), '0000-00-00 00:00:00', NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0),
-(2, 1, 'system', 0, 0, 0, 0, 'System Account to pay system needs', 'public', now(), '0000-00-00 00:00:00', NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0);
+(1, 1, 'fund', 5000000, 0, 0, 0, 'System Fund, where the non asigned money stays', 'public', now(), now(), NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0),
+(2, 1, 'system', 0, 0, 0, 0, 'System Account to pay system needs', 'public', now(), now(), NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0);
 
 
 --
@@ -710,25 +776,28 @@ INSERT INTO `rbu_authorization` (`entity_id`, `account_id`, `code`, `class`, `ti
 --
 
 INSERT INTO `rbu_notification` (`id`, `title`, `message`, `subject`, `view`) VALUES
-(1, 'New payment', 'Received {amount} from {charge_account_number} by {charge_user_name} to your account {deposit_account_number}, subject: "{subject}".', '[DEMOS] New Payment', NULL),
-(2, 'New charge', 'Paid {amount} from your account {charge_account_number} to {deposit_account_number} ({deposit_user_name}), subject: "{subject}".', '[DEMOS] New charge', NULL),
-(3, 'New salary', 'Salary assigned: {amount} "{subject}".', '[DEMOS] Salary assigned', NULL),
-(4, 'Taxes charged', 'Taxes charged: {amount} "{subject}".', '[DEMOS] Taxes Charged', NULL),
-(5, 'New pending transaction', 'You have receive a pending charge from your account {charge_account_number} to {deposit_account_number} ({deposit_user_name}) with an amount of {amount}, subject: "{subject}".', '[DEMOS] New pending transaction', NULL),
+(1, 'New payment', 'Received {amount} from {charge_account_number} by {charge_user_name} to your account {deposit_account_number}, subject: \"{subject}\".', '[DEMOS] New Payment', NULL),
+(2, 'New charge', 'Paid {amount} from your account {charge_account_number} to {deposit_account_number} ({deposit_user_name}), subject: \"{subject}\".', '[DEMOS] New charge', NULL),
+(3, 'New salary', 'Salary assigned: {amount} \"{subject}\".', '[DEMOS] Salary assigned', NULL),
+(4, 'Taxes charged', 'Taxes charged: {amount} \"{subject}\".', '[DEMOS] Taxes Charged', NULL),
+(5, 'New pending transaction', 'You have receive a pending charge from your account {charge_account_number} to {deposit_account_number} ({deposit_user_name}) with an amount of {amount}, subject: \"{subject}\".', '[DEMOS] New pending transaction', NULL),
 (6, 'advice not connected', 'Advice not connected so you will not get salary', 'Advice not connected', NULL),
-(7, 'New user joined to advertisement', '{user_name} has signed up in the advertisement: "{ad_title}".|<b>N_{user_name}</b> {user_name} have signed up in the advertisement: "{ad_title}".', '[DEMOS Market] {user_name} signed up in {ad_title}', NULL),
-(8, 'New market comunication', 'New communication by {user_name} in advert {ad_title}|New communications by {user_name} in advert {ad_title}', '[DEMOS Market] New Market comunication in "{ad_title}"', NULL),
-(9, 'Advertisement is to expire', 'Advertisement {title} is to expire, update expiration time to keep it available.', '[DEMOS Market] Advert "{title}" is to expire soon', NULL),
-(10, 'Advert expired', 'The advertisement "{title}" has expired, update it to keep it available.', '[DEMOS Market] Advert "{title}" expired', NULL),
+(7, 'New user joined to advertisement', '{user_name} has signed up in the advertisement: \"{ad_title}\".|<b>N_{user_name}</b> {user_name} have signed up in the advertisement: \"{ad_title}\".', '[DEMOS Market] {user_name} signed up in {ad_title}', NULL),
+(8, 'New market comunication', 'New communication by {user_name} in advert {ad_title}|New communications by {user_name} in advert {ad_title}', '[DEMOS Market] New Market comunication in \"{ad_title}\"', NULL),
+(9, 'Advertisement is to expire', 'Advertisement {title} is to expire, update expiration time to keep it available.', '[DEMOS Market] Advert \"{title}\" is to expire soon', NULL),
+(10, 'Advert expired', 'The advertisement \"{title}\" has expired, update it to keep it available.', '[DEMOS Market] Advert \"{title}\" expired', NULL),
 (11, 'New comment in advertisement', 'New communication in advert {ad_title}|New communications in advert {ad_title}', '[DEMOS Market] New comment in {ad_title}', NULL),
 (12, 'New advert status', 'New advert status', 'New advert status', NULL),
 (13, 'New contribution communication', 'New contribution communication', 'New contribution communication', NULL),
-(14, 'System Transaction', 'Moved {amount} from {charge_account_number} to {deposit_account_number}, subject: "{subject}".', '[DEMOS] System Transaction', NULL),
-(15, 'New Payment', 'Received {amount} from {charge_account_number} ({charge_user_name}) to your account {deposit_account_number}, subject: "{subject}".', '[DEMOS] New Payment', NULL),
-(16, 'New charge', 'Paid {amount} from your account {charge_account_number} to {deposit_account_number} ({deposit_user_name}), subject: "{subject}".', '[DEMOS] New charge', NULL),
-(17, 'New salary', 'Salary assigned: {amount} "{subject}".\r\n\r\n Demos es un sistema que busca la reciprocidad entre las personas participantes.\r\n\r\n Llevas un tiempo sin aportar, por eso te animamos a que busques dónde puedes ayudar. Tu sueldo se restablecerá tan pronto como lo consigas.\r\n\r\n ¡Ánimo!', '[DEMOS] Salary assigned', NULL),
-(18,'Welcome to Demos, this is your first salary', 'Salary assigned: {amount} "{subject}".\r\n\r\n Bienvenid@ a Demos. Demos es un sistema que busca la reciprocidad entre las personas participantes, al principio puede parecer complicado pero verás que pronto te familiarizas con la herramienta.\r\n\r\n Este es tu primer sueldo en Demos y ya puedes hacer uso de estos demos. Pero ten en cuenta que en Demos se prioriza que \'demos\' así que una vez consigas aportar a otros usuarios a través de Demos tu siguiente sueldo podrá ser mayor al sueldo mínimo (y será aún mayor si consigues aportar más de lo que consumas).\r\n\r\n ¡Bienvenid@!', '[DEMOS] Salary assigned', NULL),
-(19,'New salary', 'Salary assigned: {amount} "{subject}".\r\n\r\n Demos es un sistema que busca la reciprocidad entre las personas participantes.\r\n\r\n Vemos que no has conseguido aportar a otros usuarios a través de Demos todavía por eso tu sueldo se mantiene en el mínimo. Tu sueldo se restablecerá a los valores habituales tan pronto como lo consigas.\r\n\r\n ¡Ánimo!', '[DEMOS] Salary assigned', NULL);
+(14, 'System Transaction', 'Moved {amount} from {charge_account_number} to {deposit_account_number}, subject: \"{subject}\".', '[DEMOS] System Transaction', NULL),
+(15, 'New Payment', 'Received {amount} from {charge_account_number} ({charge_user_name}) to your account {deposit_account_number}, subject: \"{subject}\".', '[DEMOS] New Payment', NULL),
+(16, 'New charge', 'Paid {amount} from your account {charge_account_number} to {deposit_account_number} ({deposit_user_name}), subject: \"{subject}\".', '[DEMOS] New charge', NULL),
+(17, 'New salary', 'Salary assigned: {amount} \"{subject}\".\r\n\r\n Demos es un sistema que busca la reciprocidad entre las personas participantes.\r\n\r\n Llevas un tiempo sin aportar, por eso te animamos a que busques dónde puedes ayudar. Tu sueldo se restablecerá tan pronto como lo consigas.\r\n\r\n ¡Ánimo!', '[DEMOS] Salary assigned', NULL),
+(18, 'Welcome to Demos, this is your first salary', 'Salary assigned: {amount} \"{subject}\".\r\n\r\n Bienvenid@ a Demos. Demos es un sistema que busca la reciprocidad entre las personas participantes, al principio puede parecer complicado pero verás que pronto te familiarizarás con la herramienta.\r\n\r\n Este es tu primer sueldo en Demos y ya puedes hacer uso de estos demos. Pero ten en cuenta que en Demos se prioriza que \'demos\' así que una vez que consigas aportar a otros usuarios a través de Demos tu siguiente sueldo podrá ser mayor al sueldo mínimo (y será aún mayor si consigues aportar más de lo que consumas).\r\n\r\n ¡Bienvenid@!', '[DEMOS] Salary assigned', NULL),
+(19, 'New salary', 'Salary assigned: {amount} \"{subject}\".\r\n\r\n Demos es un sistema que busca la reciprocidad entre las personas participantes.\r\n\r\n Vemos que no has conseguido aportar a otros usuarios a través de Demos todavía por eso tu sueldo se mantiene en el mínimo. Tu sueldo se restablecerá a los valores habituales tan pronto como lo consigas.\r\n\r\n ¡Ánimo!', '[DEMOS] Salary assigned', NULL),
+(20, 'Broadcast message', '{text}', '[DEMOS] Nuevo mensaje', NULL),
+(21, 'New market ad', 'New market ad \"{title}\"', '[DEMOS Market] New Market add \"{title}\"', NULL),
+(22, 'New contact by e-mail', 'You have been contacted by a user of monedademos, please check your e-mail someone is waiting for your answer', '[DEMOS] New contact by e-mail', NULL);
 
 
 --
